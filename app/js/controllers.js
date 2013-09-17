a4b23329b2abde7610416e78bf769dd2ab8a33ec
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-09-17 18:14:38 +0100

    added step 2 to form

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,6 +5,7 @@
 spruce.
   controller('NewEntryCtrl', ['$scope', function($scope) {
   	$scope.update = function(thing){
+  		$scope.next = "andale";
   		console.log("thanks boss, I've recieved the thing: " + angular.toJson(thing, true));
   	}
 
