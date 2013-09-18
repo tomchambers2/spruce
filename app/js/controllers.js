30c02965533c945ef25bb9b61198032c3d4258ec
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-09-18 09:50:57 +0100

    added extra steps

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,7 @@
 
 spruce.
   controller('NewEntryCtrl', ['$scope', function($scope) {
+  	$scope.stage = 1;
   	$scope.update = function(thing){
   		$scope.next = "andale";
   		console.log("thanks boss, I've recieved the thing: " + angular.toJson(thing, true));
