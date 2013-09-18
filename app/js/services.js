ce38774a2f343c4f99dfb8afe31c01c4a79b2b3e
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-09-18 11:22:03 +0100

    added new service

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,12 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+spruce.
+	service();
+
+angular.module('phonecatServices', ['ngResource']).
+    factory('Phone', function($resource){
+  return $resource('phones/:phoneId.json', {}, {
+    query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+  });
+});
