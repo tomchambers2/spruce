15e3d7fa21138da92aefd09f66961282f2e887fc
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-09-19 17:24:40 +0100

    added form capture

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,9 +6,12 @@ spruce.
   controller('NewEntryCtrl', ['$scope', '_Parse', function($scope, _parse) {
   	$scope.stage = 1;
   	$scope.curEmotion = '';
-  	$scope.cbtEntry = { emotions: [], negativeBeliefs: []};
-  	var CbtEntry = _parse.Object.extend("CbtEntry");
+  	$scope.newThought = ''
+  	$scope.cbtEntry = { emotions: [], negativeBeliefs: {}};
+  	$scope.distortions = ["distortion a", "distortion b", "distortion c", "distortion d", "distortion e", "distortion f"];
+    var CbtEntry = _parse.Object.extend("CbtEntry");
   	var newEntry = new CbtEntry();
+  	var negBeliefCopy;
 
   	var init = function(obj){
   		obj.save(null, {
@@ -25,19 +28,48 @@ spruce.
   		});
   	}
 
+  	$scope.nextNegBelief = function(curDistortion, curNegBelief, newThought){
+  		//move on to next belief
+  		if($scope.distortions.indexOf(curDistortion) == $scope.distortions.length - 1){
+  			var nextNeg = negBeliefCopy.pop();
+  			$scope.curDistortion = $scope.distortions[0];
+  			if(nextNeg === undefined){
+  				$scope.stage = 5;
+  			}else{
+  				$scope.negBelief = nextNeg;
+  			}
+  		}
+  		//move on to next distortion
+  		else{
+  			if(newThought.length>2){
+	  			$scope.cbtEntry.negativeBeliefs[$scope.negBelief].push({'distortion': $scope.curDistortion, 'why': $scope.newthought});
+  			}
+			$scope.curDistortion = $scope.distortions[$scope.distortions.indexOf(curDistortion)+1];
+  		}
+  		//clear justification
+  		//$scope.newthought = '';
+
+  	}
 
   	$scope.addEmotion = function(newEmotion){
   		$scope.cbtEntry.emotions.push(newEmotion);
       $scope.newEmotion = "";
   	}
   	$scope.addBelief = function(negativeBelief){
-  		$scope.cbtEntry.negativeBeliefs.push(negativeBelief);
+  		$scope.cbtEntry.negativeBeliefs[negativeBelief] = [];
+  		$scope.negativeBeliefsCopy = Object.keys($scope.cbtEntry.negativeBeliefs);
   	}
   	$scope.$watch('stage', function(newValue, oldValue){
   		if(newValue === oldValue){ return; }
 
-  		if(newValue == 2){init(newEntry);}
+  		//if(newValue == 2){init(newEntry);}
 
+  		if(newValue == 4){
+  			//initialise first neg belief
+  			negBeliefCopy = $scope.negativeBeliefsCopy;
+  			$scope.curDistortion = $scope.distortions[0];
+  			$scope.negBelief = negBeliefCopy.pop();
+  		}
   		newEntry.set('stageCompleted', $scope.stage);
   		for (var prop in $scope.cbtEntry) {
   		   if($scope.cbtEntry.hasOwnProperty(prop)){
@@ -45,7 +77,7 @@ spruce.
   		   }
   		}
 
-  		newEntry.save();
+  		//newEntry.save();
   	});
 
 
