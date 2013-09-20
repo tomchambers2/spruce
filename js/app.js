f46d9b08cfa60c7b4a613cdd45121f3efbf36d85
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-09-20 14:38:53 +0100

    pages setup

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* App Module */
+
+var spruce = angular.module('spruce', [])
+
+spruce.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/entries/new', {templateUrl: 'partials/new-cbt-entry.html',   controller: 'NewEntryCtrl'}).
+      when('/', {templateUrl: 'partials/index2.html',   controller: ''}).
+      when('/get-started', {templateUrl: 'partials/sign-in.html',   controller: 'RegistrationCtrl'}).
+      when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: ''}).
+      otherwise({redirectTo: '/'});
+}]);
