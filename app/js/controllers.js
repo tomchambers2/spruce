f6750ee482420ed806e62583c3948ad675ceefab
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-09-24 11:43:28 +0100

    added in accordion

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,22 +6,20 @@ spruce.
   controller('NewEntryCtrl', ['$scope', '_Parse', '$routeParams', '$location', 'sharedState', function($scope, _parse, $routeParams, $location, sharedState) {
   	$scope.stage = 1;
   	$scope.curEmotion = '';
-  	$scope.newthought = '';
+  	$scope.newthoughts = {};
   	$scope.concern = '';
   	$scope.cbtEntry = { concern: '', emotions: [], negativeBeliefs: {}};
-    $scope.newDistortions =
+    $scope.distortions =
     {"All-Or-Nothing Thinking": "Are you seeing things in black and white? Or are there a range of possible interpretations of the situation that you haven't considered?"
     ,"Assuming the Worst Case Scenario": "What have you assumed about the consequences of this situation? If you think about the range of possible outcomes, will it really be as bad as you imagine? What are some other possible ways in which this could go?"
-    ,"Mental Filter": "Are you focussing in on a particular aspect of your life to the exclusion of everything else?"
+    ,"Mental Filtering": "Are you focussing in on a particular aspect of your life to the exclusion of everything else?"
     ,"Mind Reading": "Are you assuming something about what another person is thinking, or do you have evidence to support this? How certain you can be about the contents of someone else’s mind? "
     ,"Predicting the Future": "Is this thought trying to predict the future? Although you can make guesses about what will happen, it’s a good idea to remember that nothing in this thought is guaranteed to occur."
     ,"Labelling a Person": "By saying ‘That person is an X’ or 'I’m a Y', are you allowing a single action to define an entire life? Think about how many things you’ve done in your life and see how much of your life this incident actually is."
-    ,"Personalization": "Is this thought about assigning responsibility? Think about to what extent you or that person was really responsible, and what might have been down to circumstance or chance."
-    ,"Should Statements": "Are you using words like ‘must’ or ‘should’? Consider whether your expectations need to be realigned. You’ll either have to adjust your expectations to match reality, or always feel let down by others (or yourself)"
+    ,"Assigning Responsibility": "Is this thought about assigning responsibility? Think about to what extent you or that person was really responsible, and what might have been down to circumstance or chance."
+    ,"Unrealistic Expectation": "Are you using words like ‘must’ or ‘should’? Consider whether your expectations need to be realigned. You’ll either have to adjust your expectations to match reality, or always feel let down by others (or yourself)"
     }
 
-  	$scope.distortions = ["Are there only two possible outcomes to this situation? Is there a bigger range of options that you haven’t considered? If something is going to go badly, is it possible that it will go badly in a minor way, rather than an extreme way? This also applies to the reactions of others.", "What have you assumed about the consequences of this situation? If you think about the range of possible outcomes, will it really be as bad as you imagine? What are some other possible ways in which this could go?", "Are you focussing in on a particular aspect of your life to the exclusion of everything else?", "Think about the consequences that this thought states or implies. Think about a scale of awful things and see where this incident fits in context. How awful is it when you compare it to truly terrible things? Will it really be the end of the world?", "Are you assuming something about what another person is thinking? How certain you can be about the contents of someone else’s mind? Has it been explicitly stated or is this just an assumption?", "Is this thought trying to predict the future? Although you can make guesses about what will happen, you can’t say anything for certain, so it’s a good idea to remember that nothing in this thought is guaranteed to occur.","Is this a thought about your self worth? This is a statement like ‘I’m useless’ or ‘I’m a bad person because...’. This is something that we often do when we allow a single action or event to entirely define our lives. Think about how many things you’ve done in your life and see how much of your life this incident actually is.","Is this thought about assigning responsibility? Are you trying to make yourself or someone else solely responsible for what happened in this situation? Think about to what extent you or that person was really responsible, and what might have been down to chance or circumstances.","Is this thought about doggedly applying a rule? You might be using a word like ‘must’ or ‘should’ followed by something you feel obliged to do. Rules can be useful reminders of things that are generally useful, but that’s all they are. If a rule doesn’t help you live your life, why keep it?"];
-  	$scope.distortionsCopy = ["Are there only two possible outcomes to this situation? Is there a bigger range of options that you haven’t considered? If something is going to go badly, is it possible that it will go badly in a minor way, rather than an extreme way? This also applies to the reactions of others.", "What have you assumed about the consequences of this situation? If you think about the range of possible outcomes, will it really be as bad as you imagine? What are some other possible ways in which this could go?", "Are you focussing in on a particular aspect of your life to the exclusion of everything else?", "Think about the consequences that this thought states or implies. Think about a scale of awful things and see where this incident fits in context. How awful is it when you compare it to truly terrible things? Will it really be the end of the world?", "Are you assuming something about what another person is thinking? How certain you can be about the contents of someone else’s mind? Has it been explicitly stated or is this just an assumption?", "Is this thought trying to predict the future? Although you can make guesses about what will happen, you can’t say anything for certain, so it’s a good idea to remember that nothing in this thought is guaranteed to occur.","Is this a thought about your self worth? This is a statement like ‘I’m useless’ or ‘I’m a bad person because...’. This is something that we often do when we allow a single action or event to entirely define our lives. Think about how many things you’ve done in your life and see how much of your life this incident actually is.","Is this thought about assigning responsibility? Are you trying to make yourself or someone else solely responsible for what happened in this situation? Think about to what extent you or that person was really responsible, and what might have been down to chance or circumstances.","Is this thought about doggedly applying a rule? You might be using a word like ‘must’ or ‘should’ followed by something you feel obliged to do. Rules can be useful reminders of things that are generally useful, but that’s all they are. If a rule doesn’t help you live your life, why keep it?"];
     var CbtEntry = _parse.Object.extend("CbtEntry");
   	var newEntry = new CbtEntry();
   	var negBeliefCopy;
@@ -38,7 +36,7 @@ spruce.
 
   		}
   		setTimeout(function(){
-  			$(document).foundation('joyride', 'start');
+  			//$(document).foundation('joyride', 'start');
   		},500);
 
   	}
@@ -69,28 +67,21 @@ spruce.
   		$('#introModal').foundation('reveal', 'close');
   	}
   	$scope.nextNegBelief = function(curDistortion, curNegBelief, newThought){
-  		//move on to next belief
-  		if($scope.distortions.indexOf(curDistortion) == $scope.distortions.length - 1){
-  			var nextNeg = negBeliefCopy.pop();
-  			$scope.curDistortion = $scope.distortions[0];
-  			if(nextNeg === undefined){
-  				$scope.stage = '5';
-  			}else{
-  				$scope.negBelief = nextNeg;
-  			}
-  		}
-  		//move on to next distortion
-  		else{
-  			if(newThought != false && newThought.length>2){
-	  			$scope.cbtEntry.negativeBeliefs[$scope.negBelief].push({'distortion': $scope.curDistortion, 'why': newThought});
-  			}
-			$scope.curDistortion = $scope.distortions[$scope.distortions.indexOf(curDistortion)+1];
-  		}
+  			// var nextNeg = negBeliefCopy.pop();
+  			// if(nextNeg === undefined){
+  			// 	$scope.stage = '5';
+  			// }else{
+  			// 	$scope.negBelief = nextNeg;
+  			// }
+        // refactor, newThoughts will prolly be array.
+  			// if(newThought != false && newThought.length>2){
+	  		// 	$scope.cbtEntry.negativeBeliefs[$scope.negBelief].push({'distortion': $scope.curDistortion, 'why': newThought});
+  			// }
 
   	}
-
+    //Deprecated
   	$scope.getDistortionID = function(distortion){
-  		return $scope.distortionsCopy.indexOf(distortion)+1;
+  		return 1;
   	}
   	$scope.addEmotion = function(newEmotion){
   		$scope.cbtEntry.emotions.push(newEmotion);
@@ -114,7 +105,6 @@ spruce.
   		if(newValue == 4){
   			//initEntryObjialise first neg belief
   			negBeliefCopy = $scope.negativeBeliefsCopy;
-  			$scope.curDistortion = $scope.distortions[0];
   			$scope.negBelief = negBeliefCopy.pop();
   		}
   		newEntry.set('stageCompleted', String(oldValue));
