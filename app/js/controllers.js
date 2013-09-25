d35bb39fed443b507118d858b8f984d2a23ccc2e
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-09-25 13:25:49 +0100

    textbox in accordion

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,21 +5,24 @@
 spruce.
   controller('NewEntryCtrl', ['$scope', '_Parse', '$routeParams', '$location', 'sharedState', function($scope, _parse, $routeParams, $location, sharedState) {
   	$scope.stage = 1;
+    $(document).foundation();
   	$scope.curEmotion = '';
     $scope.newThoughts = [];
+    $scope.nextBelief = 'Done';
     $scope.changeStep = {reformedThought: ''}
     $scope.firstDistortionSelected = {'state': false, 'focusText': false};
   	$scope.concern = '';
   	$scope.cbtEntry = { concern: '', emotions: [], negativeBeliefs: {}};
     $scope.distortions =
-    {"Could there be more options than you've assumed?": "Are you seeing things in black and white? Are there a range of possible interpretations of the situation that you haven't considered?"
-    ,"Have you possibly assumed the worst?": "What have you assumed about the consequences of this situation? If you think about the range of possible outcomes, will it really be as bad as you imagine? What are some other possible ways in which this could go?"
-    ,"Have you focused in on a single aspect of life?": "Are you focussing in on a particular aspect of your life to the exclusion of everything else?"
-    ,"Are you trying to read someone's mind?": "Are you assuming something about what another person is thinking, or do you have evidence to support this? How certain you can be about the contents of someone else’s mind? "
-    ,"Are you trying to predict the future?": "Is this thought trying to predict the future? Although you can make guesses about what will happen, it’s a good idea to remember that nothing in this thought is guaranteed to occur."
-    ,"Have you given yourself or someone else a label?": "By saying ‘That person is an X’ or 'I’m a Y', are you allowing a single action to define an entire life? Think about how many things you’ve done in your life and see how much of your life this incident actually is."
-    ,"Have you assigned responsibility?": "Is this thought about assigning responsibility? Think about to what extent you or that person was really responsible, and what might have been down to circumstance or chance."
-    ,"Do you have unrealistic expectations?": "Are you using words like ‘must’ or ‘should’? Consider whether your expectations need to be realigned. You’ll either have to adjust your expectations to match reality, or always feel let down by others (or yourself)"
+    {
+    "Assuming the Worst Case Scenario": {'fullDescription': "What have you assumed about the consequences of this situation? If you think about the range of possible outcomes, will it really be as bad as you imagine? What are some other possible ways in which this could go?", 'shortDescription': "Have you assumed the worst?"}
+    ,"Mental Filtering": {'fullDescription': "Are you focussing in on a particular aspect of your life to the exclusion of everything else?", 'shortDescription': "Have you focused in on a single aspect of life?"}
+    ,"Mind-Reading": {'fullDescription': "Are you assuming something about what another person is thinking, or do you have evidence to support this? How certain you can be about the contents of someone else’s mind? ", 'shortDescription': "Are you trying to read someone's mind?"}
+    ,"Predicting the Future": {'fullDescription': "Is this thought trying to predict the future? Although you can make guesses about what will happen, it’s a good idea to remember that nothing in this thought is guaranteed to occur.", 'shortDescription': "Are you trying to predict the future?"}
+    ,"Labelling a Person": {'fullDescription': "By saying ‘That person is an X’ or 'I’m a Y', are you allowing a single action to define an entire life? Think about how many things you’ve done in your life and see how much of your life this incident actually is.", 'shortDescription': "Have you given yourself or someone else a label?"}
+    ,"Assigning Responsibility/Blaming": {'fullDescription': "Is this thought about assigning responsibility? Think about to what extent you or that person was really responsible, and what might have been down to circumstance or chance.", 'shortDescription': "Are you blaming or assigning responsibility?"}
+    ,"Unrealistic Expectations": {'fullDescription': "Are you using words like ‘must’ or ‘should’? Consider whether your expectations need to be realigned. You’ll either have to adjust your expectations to match reality, or always feel let down by others (or yourself)", 'shortDescription': 'Do you have unrealistic expectations?'}
+    ,"All-Or-Nothing Thinking": {'fullDescription': "Are you seeing things in black and white? Are there a range of possible interpretations of the situation that you haven't considered?", 'shortDescription': "Are you seeing things as only black & white?"}
     };
     if (_parse.User.current()) {
       //_parse.User.logOut()
@@ -49,9 +52,7 @@ spruce.
         },500)
 
   		}
-  		setTimeout(function(){
-  			//$(document).foundation('joyride', 'start');
-  		},500);
+
 
   	}
   	init();
@@ -84,27 +85,18 @@ spruce.
         console.log($scope.newThoughts);
         console.log($scope.changeStep.reformedThought);
         $scope.cbtEntry.negativeBeliefs[$scope.negBelief] = {distortions: $scope.newThoughts, newThought: $scope.changeStep.reformedThought};
-        var nextNeg  = "";
+        var nextNeg  = negBeliefCopy.pop();;
+        /* refactor this setting next neg business, shuoldn't be dependant on order of code*/
+        $scope.setNextBelief();
         $scope.newThoughts = [];
         $scope.changeStep.reformedThought = ''
         $scope.firstDistortionSelected.state =  $scope.firstDistortionSelected.focusText = false;
-        if(nextNeg === ""){
+        if(nextNeg === undefined){
           $scope.stage = '5';
         }
         else{
           $scope.negBelief = nextNeg;
         }
-  			// var nextNeg = negBeliefCopy.pop();
-  			// if(nextNeg === undefined){
-  			// 	$scope.stage = '5';
-  			// }else{
-  			// 	$scope.negBelief = nextNeg;
-  			// }
-        // refactor, newThoughts will prolly be array.
-  			// if(newThought != false && newThought.length>2){
-	  		// 	$scope.cbtEntry.negativeBeliefs[$scope.negBelief].push({'distortion': $scope.curDistortion, 'why': newThought});
-  			// }
-
   	}
     //Deprecated
   	$scope.getDistortionID = function(distortion){
@@ -117,6 +109,14 @@ spruce.
       }
       $scope.newEmotion = "";
   	}
+    $scope.setNextBelief = function(){
+      if(negBeliefCopy[0] === undefined){
+        $scope.nextBelief = 'Done'
+      }
+      else{
+        $scope.nextBelief = 'Next Belief: ' + negBeliefCopy[0];
+      }
+    }
   	$scope.addBelief = function(negativeBelief){
       if (negativeBelief) {
     		$scope.cbtEntry.negativeBeliefs[negativeBelief] = [];
@@ -140,6 +140,7 @@ spruce.
   			//initEntryObjialise first neg belief
   			negBeliefCopy = $scope.negativeBeliefsCopy;
   			$scope.negBelief = negBeliefCopy.pop();
+        $scope.setNextBelief();
   		}
   		newEntry.set('stageCompleted', String(oldValue));
   		for (var prop in $scope.cbtEntry) {
