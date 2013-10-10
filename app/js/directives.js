93d638738f653d667e862e01a2e1c84076dc56ca
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-10-10 11:43:20 +0100

    added greater copy

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,3 +2,36 @@
 
 /* Directives */
 
+spruce.
+	directive('scrollFix', ['$window', '$interval', function ($window, $interval) {
+	  return function (scope, element, attributes) {
+	    var didScroll = false;
+	    console.log('it loaded')
+		function isPastView(elem)
+		{
+		    var docViewTop = angular.element($window).scrollTop();
+		    var elemTop = elem.offset().top;
+
+		    return (elemTop >= docViewTop);
+		}
+
+	    angular.element($window).bind('scroll', function() {
+	        didScroll = true;
+	        console.log('it scrolled')
+	    });
+
+	    $interval(function() {
+	        if ( didScroll ) {
+
+	            didScroll = false;
+	            if(isPastView(element)){
+	            	element.addClass('stick-it');
+	            }else{
+	            	element.removeClass('stick-it');
+	            }
+
+	        }
+	    }, 250);
+	  }
+
+	}]);
