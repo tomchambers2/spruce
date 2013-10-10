b25129e24362db9e45585bb1e2487263dc47d82c
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-10-10 13:47:51 +0100

    sticky bar

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,28 +3,28 @@
 /* Directives */
 
 spruce.
-	directive('scrollFix', ['$window', '$interval', function ($window, $interval) {
+	directive('scrollfix', ['$window', function ($window, $interval) {
 	  return function (scope, element, attributes) {
-	    var didScroll = false;
-	    console.log('it loaded')
-		function isPastView(elem)
+	    var didScroll = false, elemTop = undefined;
+		function isPastView()
 		{
+			if(elemTop == undefined){
+				elemTop = element.offset().top;
+			}
 		    var docViewTop = angular.element($window).scrollTop();
-		    var elemTop = elem.offset().top;
 
-		    return (elemTop >= docViewTop);
+		    return (elemTop <= docViewTop);
 		}
 
 	    angular.element($window).bind('scroll', function() {
 	        didScroll = true;
-	        console.log('it scrolled')
 	    });
 
-	    $interval(function() {
+	    setInterval(function() {
 	        if ( didScroll ) {
 
 	            didScroll = false;
-	            if(isPastView(element)){
+	            if(isPastView()){
 	            	element.addClass('stick-it');
 	            }else{
 	            	element.removeClass('stick-it');
